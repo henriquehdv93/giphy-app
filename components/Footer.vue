be7ee5d16992eddba9df427ab33c837deec1248a
Henrique Vargas <henrique@henriquevargas.com.br>
2021-04-17 23:38:43 -0300

    fixed github url footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="footer">
-    <a href="https://www.github.com/henriquehdv93/hotelflow_teste/">
-      <img src="/images/github.svg" alt="Github" title="Acesse o Github do projeto">
+    <a href="https://github.com/henriquehdv93/giphy-app" target="_blank" title="Acesse o projeto no Github" rel="nofollow">
+      <img src="/images/github.svg" alt="Github" title="Acesse o projeto no Github">
       <span>Acesse o projeto no Github</span>
     </a>
     <p>Giphy App - desenvolvido por <a href="https://www.henriquevargas.com.br" rel="nofollow" title="Acesse o portfólio de Henrique">Henrique Vargas</a> - 2021. Todos os direitos reservados.</p>
